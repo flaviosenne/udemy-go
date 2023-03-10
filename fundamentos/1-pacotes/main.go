2a5581ff68fa42dc68291fa9950f24e0d4266c45
joao flavio <flaviosenne123@gmail.com>
2023-03-10 19:14:20 -0300

    adicionar pacote externo

--- fundamentos/1-pacotes/main.go
+++ fundamentos/1-pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("escrevendo do main")
 	auxiliar.Escrever()
+
+	err := checkmail.ValidateFormat("joao@gmail.com")
+	fmt.Println(err)
 }
