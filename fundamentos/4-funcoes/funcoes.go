c033ed53ca906cf3e5cc8f32291460f633b9b8bc
joao flavio <flaviosenne123@gmail.com>
2023-03-18 17:57:25 -0300

    funcoes

---
+++ fundamentos/4-funcoes/funcoes.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(10, 20)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		return "Função F " + txt
+	}
+
+	resultado := f("mmaais texto")
+	fmt.Println(resultado)
+
+	resultadoSoma, _ := calculos(10, 15)
+	fmt.Println(resultadoSoma)
+}
